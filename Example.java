5c0f980742ab99f64ecec7a7493f236778b0cc1a
taniagwl <168339039+taniagwl@users.noreply.github.com>
2024-06-07 15:42:19 +0530

    Update Example.java

--- Example.java
+++ Example.java
@@ -2,7 +2,7 @@
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
-public class DevOpsExample {
+public class Example {
   public static void main(String[] args) {
     System.out.println("Welcome to DevOps.");
   }
